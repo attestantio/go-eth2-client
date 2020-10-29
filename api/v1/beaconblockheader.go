3db2bad0611e8372cfae7bfc4ec9f234628d52f1
Jim McDonald <Jim@mcdee.net>
2020-10-29 22:45:02 +0000

    Add beacon block header

---
+++ api/v1/beaconblockheader.go
@@ -0,0 +1,86 @@
+// Copyright © 2020 Attestant Limited.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	spec "github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/pkg/errors"
+)
+
+// BeaconBlockHeader is the data providing information about beacon blocks.
+type BeaconBlockHeader struct {
+	// Root is the root of the beacon block.
+	Root []byte
+	// Canonical is true if the block is considered canonical.
+	Canonical bool
+	// Header is the beacon block header.
+	Header *spec.SignedBeaconBlockHeader
+}
+
+// beaconBlockHeaderJSON is the spec representation of the struct.
+type beaconBlockHeaderJSON struct {
+	Root      string                        `json:"root"`
+	Canonical bool                          `json:"canonical"`
+	Header    *spec.SignedBeaconBlockHeader `json:"header"`
+}
+
+// MarshalJSON implements json.Marshaler.
+func (b *BeaconBlockHeader) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&beaconBlockHeaderJSON{
+		Root:      fmt.Sprintf("%#x", b.Root),
+		Canonical: b.Canonical,
+		Header:    b.Header,
+	})
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (b *BeaconBlockHeader) UnmarshalJSON(input []byte) error {
+	var err error
+
+	var beaconBlockHeaderJSON beaconBlockHeaderJSON
+	if err = json.Unmarshal(input, &beaconBlockHeaderJSON); err != nil {
+		return errors.Wrap(err, "invalid JSON")
+	}
+	if beaconBlockHeaderJSON.Root == "" {
+		return errors.New("root missing")
+	}
+	if b.Root, err = hex.DecodeString(strings.TrimPrefix(beaconBlockHeaderJSON.Root, "0x")); err != nil {
+		return errors.Wrap(err, "invalid value for root")
+	}
+	if len(b.Root) != rootLength {
+		return fmt.Errorf("incorrect length %d for root", len(b.Root))
+	}
+
+	b.Canonical = beaconBlockHeaderJSON.Canonical
+	if beaconBlockHeaderJSON.Header == nil {
+		return errors.New("header missing")
+	}
+	b.Header = beaconBlockHeaderJSON.Header
+
+	return nil
+}
+
+// String returns a string version of the structure.
+func (b *BeaconBlockHeader) String() string {
+	data, err := json.Marshal(b)
+	if err != nil {
+		return fmt.Sprintf("ERR: %v", err)
+	}
+	return string(data)
+}
