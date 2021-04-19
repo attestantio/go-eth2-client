ddd10ff7f95041aed84a3cd4bc4a9627a295ea78
Jim McDonald <Jim@mcdee.net>
2021-04-19 14:45:17 +0100

    Add content-type and accept headers to HTTP requests.

--- standardhttp/v1/http.go
+++ standardhttp/v1/http.go
@@ -53,6 +53,7 @@ func (s *Service) get(ctx context.Context, endpoint string) (io.Reader, error) {
 		cancel()
 		return nil, errors.Wrap(err, "failed to create GET request")
 	}
+	req.Header.Set("Accept", "application/json")
 	resp, err := s.client.Do(req)
 	if err != nil {
 		cancel()
@@ -108,6 +109,8 @@ func (s *Service) post(ctx context.Context, endpoint string, body io.Reader) (io
 		cancel()
 		return nil, errors.Wrap(err, "failed to create POST request")
 	}
+	req.Header.Set("Content-type", "application/json")
+	req.Header.Set("Accept", "application/json")
 	resp, err := s.client.Do(req)
 	if err != nil {
 		cancel()
