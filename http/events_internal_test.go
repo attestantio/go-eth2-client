aee8b8ef53e605067685aa5c32fc79ec2a181335
Jim McDonald <Jim@mcdee.net>
2022-06-24 08:38:40 +0100

    Tidy up tests.

--- http/events_internal_test.go
+++ http/events_internal_test.go
@@ -70,6 +70,7 @@ func TestEventHandler(t *testing.T) {
 			name: "HeadGood",
 			message: &sse.Event{
 				Event: []byte("head"),
+				Data:  []byte(`{"slot":"4095940","block":"0x73d83c5f925716c9bd2d1e9c339fb99b0ec4addef3e93f6f35d4c5f1de7ae092","state":"0xead0e6eb4004576546864f10cfa4aeac31afbf96abc405a86c00cbda8f3e8ed0","epoch_transition":false,"previous_duty_dependent_root":"0xeca94cc9180212a2cff2659289cc7e6f2df08a645120e35e25d09c2ddc7db5f1","current_duty_dependent_root":"0xdda286c4a096fc8ec0d6ba9e14e688cbb046bfb33462fdf94953e75d0cea0074","execution_optimistic":false}`),
 			},
 			handler: handler,
 			handled: true,
@@ -78,6 +79,7 @@ func TestEventHandler(t *testing.T) {
 			name: "BlockGood",
 			message: &sse.Event{
 				Event: []byte("block"),
+				Data:  []byte(`{"slot":"4095943","block":"0x1c3981b7439cd2dc53dca1a99122e1cacb36a13796d426d4c8a03ba745cb0c8b","execution_optimistic":false}`),
 			},
 			handler: handler,
 			handled: true,
@@ -86,6 +88,7 @@ func TestEventHandler(t *testing.T) {
 			name: "AttestationGood",
 			message: &sse.Event{
 				Event: []byte("attestation"),
+				Data:  []byte(`{"aggregation_bits":"0x00002840403040000000020008800040008042800000020220","data":{"slot":"4095945","index":"12","beacon_block_root":"0xff27c7551bf4cfe4dc4cce00920e7a5c5074860d1dbd8aa8b3b5f888523f51ff","source":{"epoch":"127997","root":"0x38758fb180459583bd5e8e1a31711eb09e63eb92be974485397e9a2c57de2783"},"target":{"epoch":"127998","root":"0x46d4629861bd81cfc94007501b4edb1b3ca9444b41d7a98681b6c2f4bdb978bd"}},"signature":"0xacb9f562a28c4ef5b60b88678068ea51573a3237d3331dda3b2d845a0d03bc56ab2994d2deb90d9f074a8bdab59945150d0a7717e74b1bf2627f8971c81091f724c211dfce8fa16fb839c6a1bfd341ddec5e7eb88472682fd1a170e373660534"}`),
 			},
 			handler: handler,
 			handled: true,
@@ -94,6 +97,7 @@ func TestEventHandler(t *testing.T) {
 			name: "VoluntaryExitGood",
 			message: &sse.Event{
 				Event: []byte("voluntary_exit"),
+				Data:  []byte(`{"message":{"epoch":"1", "validator_index":"1"}, "signature":"0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505"}`),
 			},
 			handler: handler,
 			handled: true,
@@ -102,6 +106,7 @@ func TestEventHandler(t *testing.T) {
 			name: "FinalizedCheckpointGood",
 			message: &sse.Event{
 				Event: []byte("finalized_checkpoint"),
+				Data:  []byte(`{"block":"0x38758fb180459583bd5e8e1a31711eb09e63eb92be974485397e9a2c57de2783","state":"0x9c237b2a66df8636f816e6b2c8860ba287fc5b817d882b1be8b7111486fb4ddc","epoch":"127997","execution_optimistic":false}`),
 			},
 			handler: handler,
 			handled: true,
@@ -110,6 +115,7 @@ func TestEventHandler(t *testing.T) {
 			name: "ChainReorgGood",
 			message: &sse.Event{
 				Event: []byte("chain_reorg"),
+				Data:  []byte(`{"slot":"4100237","depth":"2","old_head_block":"0x5c988c12b7d8638c06e6b9511e09e5e28511a16a33c153413416d3fd5d95353a","new_head_block":"0x783ccb4310c0ddab3ea500f8d2b88c5ad8d6b2d601513f4ebf491066cda1d180","old_head_state":"0xdbad017808a1c5a77866fccc3e15f14a67585fff25b3a4d947ae9cc6d937b4ab","new_head_state":"0x67f0302bb939f64b1feaaa907b30c9631c0588480f2305578377bfd87df68b95","epoch":"128132","execution_optimistic":false}`),
 			},
 			handler: handler,
 			handled: true,
@@ -118,6 +124,7 @@ func TestEventHandler(t *testing.T) {
 			name: "ContributionAndProofGood",
 			message: &sse.Event{
 				Event: []byte("contribution_and_proof"),
+				Data:  []byte(`{"message":{"aggregator_index":"355177","contribution":{"slot":"4095970","beacon_block_root":"0xfe17cc29bb937740eead4b84716751b00e361891dae7c8f0e98f4deb5f753cbc","subcommittee_index":"0","aggregation_bits":"0xffffffffffffffffffffdfffffffffff","signature":"0x926e8fbf2b8599f76a42e2dd02b954853d3841577a0c68303fb9a5690f7973e95454bc1df03118d53c80e3cf13dc33490f2516aefb4cb7766a724a10dd0536811ec43b7e5f08442ef7dbc072b4b484ea1acde78e5aae8d636b06dd18677c535b"},"selection_proof":"0x84c805b21a40315dc19ea89e6d64d8f5e913d5e003a813d74a23f16add72250791a950b7508e2ea69adab8169c2800b70d26fea4cdb51f1fb96d939baaa44567eb9e96d2021799478fd3f557326a62060215be95465fcc9c49f67dd8685a20bc"},"signature":"0x8093efce898e36cab5ab2b198a48046d029b36909a29ec33ca7075f389133288c4d7e13cf3e20396612050d4aebe9212154fd5a2be4bf356e6191600d65906d5c404bd46c95ae20fe4bc5e18c6e2808c97a4572f995bf90db8aaf3fd84fb87ac"}`),
 			},
 			handler: handler,
 			handled: true,
