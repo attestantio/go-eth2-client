5a33417f219e12762e182e3844efd521299f4d5a
Jim McDonald <Jim@mcdee.net>
2024-03-19 12:35:07 +0000

    Bump version.

--- http/http.go
+++ http/http.go
@@ -58,7 +58,7 @@ func (s *Service) post(ctx context.Context, endpoint string, body io.Reader) (io
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Accept", "application/json")
 	if req.Header.Get("User-Agent") == "" {
-		req.Header.Set("User-Agent", "go-eth2-client/0.20.0")
+		req.Header.Set("User-Agent", "go-eth2-client/0.21.0")
 	}
 
 	resp, err := s.client.Do(req)
@@ -142,7 +142,7 @@ func (s *Service) post2(ctx context.Context,
 		req.Header.Set(k, v)
 	}
 	if req.Header.Get("User-Agent") == "" {
-		req.Header.Set("User-Agent", "go-eth2-client/0.20.0")
+		req.Header.Set("User-Agent", "go-eth2-client/0.21.0")
 	}
 
 	resp, err := s.client.Do(req)
