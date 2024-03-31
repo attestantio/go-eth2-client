ece29db5ce76cafc021af9cf081d1f7d23fe2a0d
pk910 <philipp@pk910.de>
2024-03-31 03:53:33 +0200

    make linter happy

--- spec/phase0/beaconstate.go
+++ spec/phase0/beaconstate.go
@@ -35,7 +35,7 @@ type BeaconState struct {
 	LatestBlockHeader           *BeaconBlockHeader
 	BlockRoots                  []Root `dynssz-size:"SLOTS_PER_HISTORICAL_ROOT,32" ssz-size:"8192,32"`
 	StateRoots                  []Root `dynssz-size:"SLOTS_PER_HISTORICAL_ROOT,32" ssz-size:"8192,32"`
-	HistoricalRoots             []Root `ssz-max:"16777216" ssz-size:"?,32"`
+	HistoricalRoots             []Root `ssz-max:"16777216"                         ssz-size:"?,32"`
 	ETH1Data                    *ETH1Data
 	ETH1DataVotes               []*ETH1Data `ssz-max:"2048"`
 	ETH1DepositIndex            uint64
