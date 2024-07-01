490a9b458def21bd291dde286febeb42baeb40b9
pk910 <philipp@pk910.de>
2024-07-01 15:22:27 +0200

    fix unrelated `golangci-lint` warning

--- multi/client.go
+++ multi/client.go
@@ -15,6 +15,7 @@ package multi
 
 import (
 	"context"
+	nethttp "net/http"
 	"strings"
 	"time"
 
@@ -170,7 +171,7 @@ func (s *Service) doCall(ctx context.Context, call callFunc, errHandler errHandl
 			log.Trace().Err(err).Msg("Potentially deactivating client due to error")
 			var apiErr *api.Error
 			switch {
-			case errors.As(err, &apiErr) && apiErr.StatusCode/100 == 4:
+			case errors.As(err, &apiErr) && apiErr.StatusCode/nethttp.StatusContinue == 4:
 				log.Trace().Err(err).Msg("Not deactivating client on user error")
 
 				return res, err
