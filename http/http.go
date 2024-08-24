5608aaa67b9ff2e5ee41280c6bd447fe4531139b
Jim McDonald <Jim@mcdee.net>
2024-08-24 17:41:36 +0100

    Allow filtering of attestationpool by committee index.

--- http/http.go
+++ http/http.go
@@ -34,7 +34,7 @@ import (
 )
 
 // defaultUserAgent is sent with requests if no other user agent has been supplied.
-const defaultUserAgent = "go-eth2-client/0.21.10"
+const defaultUserAgent = "go-eth2-client/0.21.11"
 
 // post sends an HTTP post request and returns the body.
 func (s *Service) post(ctx context.Context, endpoint string, body io.Reader) (io.Reader, error) {
