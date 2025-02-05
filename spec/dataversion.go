0d04ee26b93a4361be18606f49d3c51b849288be
pk910 <philipp@pk910.de>
2025-02-05 12:14:39 +0100

    use `epbs` as block version for eip7732

--- spec/dataversion.go
+++ spec/dataversion.go
@@ -48,7 +48,7 @@ var dataVersionStrings = [...]string{
 	"capella",
 	"deneb",
 	"electra",
-	"eip7732",
+	"epbs",
 }
 
 // MarshalJSON implements json.Marshaler.
@@ -72,7 +72,7 @@ func (d *DataVersion) UnmarshalJSON(input []byte) error {
 		*d = DataVersionDeneb
 	case `"electra"`:
 		*d = DataVersionElectra
-	case `"eip7732"`:
+	case `"epbs"`:
 		*d = DataVersionEIP7732
 	default:
 		err = fmt.Errorf("unrecognised data version %s", string(input))
