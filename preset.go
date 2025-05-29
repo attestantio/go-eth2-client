5f241b4aa8a49a0091172553d2b930e9ae5fcdaa
jacob kaufmann <jacobkaufmann18@gmail.com>
2025-05-29 17:13:27 -0600

    remove git submodule in favor of local copy of presets

--- preset.go
+++ preset.go
@@ -6,46 +6,46 @@ import "log"
 
 import "gopkg.in/yaml.v3"
 
-//go:embed consensus-specs/presets/mainnet/phase0.yaml
+//go:embed presets/mainnet/phase0.yaml
 var mainnetPhase0 []byte
 
-//go:embed consensus-specs/presets/mainnet/altair.yaml
+//go:embed presets/mainnet/altair.yaml
 var mainnetAltair []byte
 
-//go:embed consensus-specs/presets/mainnet/bellatrix.yaml
+//go:embed presets/mainnet/bellatrix.yaml
 var mainnetBellatrix []byte
 
-//go:embed consensus-specs/presets/mainnet/capella.yaml
+//go:embed presets/mainnet/capella.yaml
 var mainnetCapella []byte
 
-//go:embed consensus-specs/presets/mainnet/deneb.yaml
+//go:embed presets/mainnet/deneb.yaml
 var mainnetDeneb []byte
 
-//go:embed consensus-specs/presets/mainnet/electra.yaml
+//go:embed presets/mainnet/electra.yaml
 var mainnetElectra []byte
 
-//go:embed consensus-specs/presets/mainnet/fulu.yaml
+//go:embed presets/mainnet/fulu.yaml
 var mainnetFulu []byte
 
-//go:embed consensus-specs/presets/minimal/phase0.yaml
+//go:embed presets/minimal/phase0.yaml
 var minimalPhase0 []byte
 
-//go:embed consensus-specs/presets/minimal/altair.yaml
+//go:embed presets/minimal/altair.yaml
 var minimalAltair []byte
 
-//go:embed consensus-specs/presets/minimal/bellatrix.yaml
+//go:embed presets/minimal/bellatrix.yaml
 var minimalBellatrix []byte
 
-//go:embed consensus-specs/presets/minimal/capella.yaml
+//go:embed presets/minimal/capella.yaml
 var minimalCapella []byte
 
-//go:embed consensus-specs/presets/minimal/deneb.yaml
+//go:embed presets/minimal/deneb.yaml
 var minimalDeneb []byte
 
-//go:embed consensus-specs/presets/minimal/electra.yaml
+//go:embed presets/minimal/electra.yaml
 var minimalElectra []byte
 
-//go:embed consensus-specs/presets/minimal/fulu.yaml
+//go:embed presets/minimal/fulu.yaml
 var minimalFulu []byte
 
 var (
